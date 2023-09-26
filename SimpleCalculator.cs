0b171370c0c2103de260a3dd50f7f32bc50ba104
Timotej Stojkovski <142885508+GrowingCash322@users.noreply.github.com>
2023-09-26 20:22:29 +0200

    Add files via upload

---
+++ SimpleCalculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Calculator
+{
+    static void Main()
+    {
+        Console.WriteLine("Simple Calculator");
+        Console.WriteLine("Geben Sie zwei Zahlen ein");
+
+        double num1 = Convert.ToDouble(Console.ReadLine());
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Wählen Sie einen Vorgang aus (+, -, *, /):");
+        char operation = Console.ReadKey().KeyChar;
+
+        double result = 0;
+
+        switch (operation)
+        {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                Console.WriteLine("Invalid Vorgang");
+                return;
+        }
+
+        Console.WriteLine($"Result: {result}");
+    }
+}
